e6891633bf79a478975be05cdb3d176ed99f5851
cchristion <47944263+cchristion@users.noreply.github.com>
2023-12-20 13:58:18 +0530

    Better syntax

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ mkdir -p ${XDG_CACHE_HOME:-$HOME/.cache}/zsh ${XDG_CONFIG_HOME:-$HOME/.config}/z
 git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting ${XDG_CONFIG_HOME:-$HOME/.config}/zsh/plugins/fast-syntax-highlighting
 
 # Installing powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${XDG_CONFIG_HOME:-$HOME/.config}/zsh/plugins/powerlevel10k
+git clone --depth 1 https://github.com/romkatv/powerlevel10k.git ${XDG_CONFIG_HOME:-$HOME/.config}/zsh/plugins/powerlevel10k
